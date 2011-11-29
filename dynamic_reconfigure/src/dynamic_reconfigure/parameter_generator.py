996860c1d6194bd1bf5fcd57493929f64bb1b137
Brett Grandbois <Brett.Grandbois@csiro.au>
2011-11-29 03:18:06 +0000

    #5259 - dynamic_reconfigure: cfg file generation not producing __init__.py file correctly
    Or more appropriately, not at all.  A legacy assumption that genmsg or gensrv will generate
    an __init__.py leaves a hole when generating a dynrecon cfg but no msg or srv.
    Patch applied to write out an init to the package source root.

--- dynamic_reconfigure/src/dynamic_reconfigure/parameter_generator.py
+++ dynamic_reconfigure/src/dynamic_reconfigure/parameter_generator.py
@@ -416,3 +416,6 @@ $i.desc=$description $range"""
 
         f = open(os.path.join(self.pkgpath, "src", self.pkgname, "cfg", "__init__.py"), 'a')
         f.close()
+
+        f = open(os.path.join(self.pkgpath, "src", self.pkgname, "__init__.py"), 'a')
+        f.close()
